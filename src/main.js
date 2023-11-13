1ca0f9e7951fc43d7a254e710effab19c392e3f2
Charles Burton <charlie-246@hotmail.co.uk>
2023-11-13 12:50:09 +0100

    making final draft

--- src/main.js
+++ src/main.js
@@ -1,108 +1,159 @@
-//selecting card part
-
 let currentScore = 0;
 
 const showScore = () => {
-  document.getElementById("score").innerHTML = `${currentScore}`;
-  if (currentScore > 7.5) {
-    gameOver();
-  }
+  document.getElementById("score").innerHTML = `Your score is ${currentScore}`;
 };
 
-const gameOver = () => {
-  document.getElementById("score").innerHTML = "You have gone over 7.5";
-  document.getElementById("hitMe").disabled = true;
-};
-
-document.addEventListener("DOMContentLoaded", showScore);
-
-const createRandomNumber = () => {
-  let randomNumber = () => Math.floor(Math.random() * 11);
-  if (randomNumber > 7) {
-    return randomNumber + 2;//use return after if
+function giveUrlCard(card) {
+  switch (card) {
+    case 1:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
+    case 2:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
+    case 3:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
+    case 4:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
+    case 5:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
+    case 6:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
+    case 7:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
+    case 10:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
+    case 11:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
+    case 12:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+    default:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
   }
-  if (!randomNumber >= 12) {
-    return randomNumber;
-  }
-
-};
-  const randomCardNumber = createRandomNumber();
-
-
-const newCard = document.getElementById("hitMe");
-newCard.addEventListener("click", createRandomNumber());
-
-const countNumber = (cardNumber) => { //this was just the place holder cardNumber its only a var when the fx is called NOT in the declaration
-  if (cardNumber === 1) {
-    currentScore++;
-    return changeCard(card1); //this didnt work as when you 'return' it leaves this section of the function
-  }
-
-  if (cardNumber === 2) {
-    currentScore + 2;
-    return changeCard(card2);
-  }
-
-  if (cardNumber === 3) {
-    currentScore + 3;
-    return changeCard(card3);
-  }
-
-  if (cardNumber === 4) {
-    currentScore + 4;
-    return changeCard(card4);
-  }
-
-  if (cardNumber === 5) {
-    currentScore + 5;
-    return changeCard(card5);
-  }
-
-  if (cardNumber === 6) {
-    currentScore + 6;
-    return changeCard(card6);
-  }
-
-  if (cardNumber === 7) {
-    currentScore + 7;
-    return changeCard(card7);
-  }
-
-  if (cardNumber === 10) {
-    currentScore + 0.5;
-    return changeCard(cardSoto);
-  }
-
-  if (cardNumber === 11) {
-    currentScore + 0.5;
-    return changeCard(cardCaballo);
-  }
-
-  if (cardNumber === 10) {
-    currentScore + 0.5;
-    return changeCard(cardRey);
-  }
-};
-
-countNumber(randomCardNumber);
-
-function changeCard(card) {
-  const cardBack = document.getElementById("startCard");
-  // const cards = document.getElementById(card); //need to link this part with the random number
-  //I want to change this so that each card selected appears next to original  card
-  cardBack.src = card.scr;
-  cardBack.alt = card.alt;
 }
 
-const card1 = document.getElementById("1 de copas");
-const card2 = document.getElementById("2 de copas");
-const card3 = document.getElementById("3 de copas");
-const card4 = document.getElementById("4 de copas");
-const card5 = document.getElementById("5 de copas");
-const card6 = document.getElementById("6 de copas");
-const card7 = document.getElementById("7 de copas");
-const cardSoto = document.getElementById("Sota de copas");
-const cardCaballo = document.getElementById("Caballo de copas");
-const cardRey = document.getElementById("Rey de copas");
+function printUrlImage(urlCard) {
+  const imgElement = document.getElementById("startCard");
+  if (imgElement !== null && imgElement !== undefined) {
+    imgElement.src = urlCard;
+  }
+}
 
-cardBack.addEventListener("click", () => changeCard(card));
+function givePointCard(card) {
+  console.log(card);
+  return card <= 7 ? card : 0.5;
+}
+
+let message = "";
+function checkGame(finalScore) {
+  if (finalScore > 7.5) {
+    gameOver();
+  }
+  if (finalScore === 7.5) {
+    winGame();
+  }
+}
+
+const disableButtons = () => {
+  document.getElementById("hitMe").disabled = true;
+  document.getElementById("stick").disabled = true;
+};
+
+function winGame() {
+  const scoreElement = document.getElementById("score");
+  if (scoreElement !== null && scoreElement !== undefined) {
+    scoreElement.textContent = "¡Lo has clavado! ¡Enhorabuena!";
+  }
+  disableButtons();
+}
+
+function gameOver() {
+  document.getElementById("score").innerHTML =
+    "Game Over, better luck next time!";
+  disableButtons();
+}
+
+const giveRandomNumber = () => {
+  return Math.ceil(Math.random() * 10);
+};
+
+const giveCardNumber = (randomNumber) => {
+  if (randomNumber > 7) {
+    randomNumber = randomNumber + 2;
+  }
+  return randomNumber;
+};
+
+const addPoints = (points) => {
+  currentScore = currentScore + points;
+};
+
+function hitMe() {
+  const randomNumber = giveRandomNumber();
+
+  const cardNumber = giveCardNumber(randomNumber);
+
+  const urlCard = giveUrlCard(cardNumber);
+
+  printUrlImage(urlCard);
+
+  const points = givePointCard(cardNumber);
+
+  addPoints(points);
+
+  showScore();
+  checkGame(currentScore);
+}
+
+const newCard = document.getElementById("hitMe");
+newCard.addEventListener("click", hitMe);
+
+//STICK BUTTON
+
+function endGameMessage(finalScore) {
+  if (finalScore < 4) {
+    message = "Has sido muy conservador";
+  } else if (finalScore <= 5) {
+    message = "Te ha entrado el canguelo eh?";
+  } else if (finalScore <= 7) {
+    message = "Casi casi...";
+  } else {
+    message = "Puntuacion no reconocida";
+  }
+  document.getElementById(
+    "score"
+  ).innerHTML = `Your final score is ${currentScore}. ${message}`;
+}
+
+const stickHandle = () => {
+  endGameMessage(currentScore);
+  disableButtons();
+};
+
+const stick = document.getElementById("stick");
+stick.addEventListener("click", stickHandle);
+
+//RESET BUTTON
+
+const resetScore = () => {
+  currentScore = 0;
+  document.getElementById("score").innerHTML = `Your score is ${currentScore}`;
+};
+
+const resetButtons = () => {
+  document.getElementById("hitMe").disabled = false;
+  document.getElementById("stick").disabled = false;
+};
+
+const resetCard = () => {
+  const urlCard = giveUrlCard(0);
+  printUrlImage(urlCard);
+};
+
+const handleReset = () => {
+  resetScore();
+  resetCard();
+  resetButtons();
+};
+
+const reset = document.getElementById("reset");
+reset.addEventListener("click", handleReset);
